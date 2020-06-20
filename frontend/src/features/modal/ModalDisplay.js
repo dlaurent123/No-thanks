91ace06fd29c9200012e98fa7b71eedc75044a82
dannyl <danilovlaurent@pursuit.org>
2020-06-20 19:30:08 -0400

    create bussines display component

--- frontend/src/features/modal/ModalDisplay.js
+++ frontend/src/features/modal/ModalDisplay.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Modal from "react-modal";
 import { styles } from "./helpers";
+import BusinessDisplay from "../bussinesDisplay/BussinesDisplay";
 
 const ModalDispaly = () => {
-  return <Modal style={styles} isOpen={true}></Modal>;
+  return (
+    <Modal style={styles} isOpen={true}>
+      <BusinessDisplay />
+    </Modal>
+  );
 };
 
 export default ModalDispaly;
