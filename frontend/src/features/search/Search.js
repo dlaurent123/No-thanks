34613e2aafc59ab1b19a7e1ed914b654da1e9da1
rafidhossain1995 <rafidhossain@pursuit.org>
2020-06-20 17:26:41 -0400

    cleaned location url

--- frontend/src/features/search/Search.js
+++ frontend/src/features/search/Search.js
@@ -12,10 +12,39 @@ const[latitude, setLatitude] = useState("")
 const[longitude, setLongitude] = useState("")
 const dispatch = useDispatch()
 console.log(location)
+
+const locationURL = ()=>{
+  if(location){
+    return (`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&limit=50`)
+  }else{
+    return(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&latitude=${latitude}&longitude=${longitude}&limit=50`)
+  }
+}
+
+
+const searchLocation = async()=>{ 
+  try{
+    navigator.geolocation.getCurrentPosition(async(position)=>{
+    setLatitude(position.coords.latitude)
+    setLongitude(position.coords.longitude)   
+   
+  })
+
+  }catch(err){
+    console.log(err)
+  }
+   
+}
+
+useEffect(()=>{
+  searchLocation()
+},[])
+
 const handleSubmit = async (e) =>{
   e.preventDefault()
+  const url = locationURL()
   try{
-    let res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`,{
+    let res = await axios.get(url,{
       headers:{
         Authorization:
         "Bearer" + " " 
@@ -33,32 +62,16 @@ const handleSubmit = async (e) =>{
   }
 
 }
+console.log(latitude, longitude)
 
-const searchLocation = async()=>{ 
-  try{
-    navigator.geolocation.getCurrentPosition(async(position)=>{
-      let res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`,{
-        headers:{
-          Authorization:
-          "Bearer" + " " + "5WZsGjXG75mAeIo30zOWJ2XRbGH-SfFe5D5YowXKQ_a5fwyQI1-1w0lsDotYUs9VYMzBYJ24E5EfzA4XdFca4nrWrm9zKacT2Bcn1cGonrRPlWWQaMvg_aqCNEXtXnYx"
-        }
-      })
-      dispatch(receiveSearch(res.data.businesses))
-      debugger
-  })
-  }catch(err){
-    console.log(err)
-  }
-   
-}
 
   return (
     <form onSubmit={handleSubmit}>
       <input className="categories" value={term} placeholder="Search By Category" onChange={(e) => setTerm(e.currentTarget.value)} />
-      <input className="location" value={location} placeholder="Search by Zip Code" onChange={(e) => setLocation(e.currentTarget.value)} />
-      <button>Submit</button>
-      <h1>or search by location</h1>
-      <button onClick={searchLocation}>Search By Location</button>
+      <input className="location" value={location} placeholder="Search By City/Zipcode" onChange={(e) => setLocation(e.currentTarget.value)} />
+      <button type="submit">Submit</button>
+     
+    
 
     </form>
   );
