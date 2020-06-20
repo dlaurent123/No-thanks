a5d91d882fbeec4187908a5a2a1e172edc051aaf
Marvin <marvinthompson@pursuit.org>
2020-06-20 17:56:06 -0400

    sorting logic for results

--- frontend/src/features/searchDisplayPage/SearchDisplayPage.js
+++ frontend/src/features/searchDisplayPage/SearchDisplayPage.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchDisplay from "../searchDisplay/SearchDisplay";
 import { useSelector } from 'react-redux'
 
 const SearchDisplayPage = () => {
+  const [ positiveResults, setPositiveResults ] = useState([])
   const searchResults = useSelector(state => state.search)
 
 
@@ -11,11 +12,9 @@ const SearchDisplayPage = () => {
   // make it a select box/check box, on selected filter through searchResults if the price is equal to "$", "$$" etc
   
   const results = searchResults.map(result => {
-    if (result.rating < 4) {
+    if (result.rating <= 3) {
       return <SearchDisplay result={result}/>
-    } else {
-      return <div><h1>No bad spots near you!!</h1></div>
-    }
+    } 
   })
 
   return (
