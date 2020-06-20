3e1db6a200913a8dc648838636bba1f4570f20f3
Marvin <marvinthompson@pursuit.org>
2020-06-20 15:22:37 -0400

    added Category

--- frontend/src/features/searchDisplay/SearchDisplay.js
+++ frontend/src/features/searchDisplay/SearchDisplay.js
@@ -5,7 +5,7 @@ import '../css/SearchDisplay.css'
 
 
 const SearchDisplay = ({result}) => {
-  let { id, name, image_url, review_count, rating, price, display_phone } = result
+  let { id, name, image_url, review_count, rating, price, display_phone, categories } = result
   const storeLocation  = result.location 
   let { address1, address2, address3, city, zip_code, country, state, display_address } = storeLocation
   // const image = result.image_url
@@ -30,6 +30,7 @@ const SearchDisplay = ({result}) => {
           <h2 id={"phone"}>Contact: {display_phone}</h2>
       </div>
       <div className={"information"}>
+          <h2 id={"category"}>Category: {categories[0].title}</h2>
           <h2 id={"reviews"}>Reviews: {review_count}</h2>
           <h2 id={"rating"}>Rating: {rating}</h2>
           <h3 id={"price"}>Price: {price}</h3>
