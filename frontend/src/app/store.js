327d7a0c6dfccada28636cf26ade4a5ffd508909
dannyl <danilovlaurent@pursuit.org>
2020-06-20 23:34:26 -0400

    create bussines slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import searchReducer from "../features/search/searchSlice";
 import modal from "../features/modal/modalSlice";
 import loading from "../features/isLoading/loadingSlice";
+import bussines from "../features/bussines/bussinesSlice";
 import logger from "redux-logger";
 
 const reducer = {
   search: searchReducer,
   modal,
   loading,
+  bussines,
 };
 
 export default configureStore({
